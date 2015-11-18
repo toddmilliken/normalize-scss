5758744b4e5ac6d8a971ce4f0cb383ac3c637dd2
JohnAlbin <virtually.johnalbin@gmail.com>
2015-11-18 14:51:31 +0800

    Add tests for Compass fork version.

--- test/test_fork-versions.js
+++ test/test_fork-versions.js
@@ -22,4 +22,26 @@ describe('Fork versions', function() {
       });
     });
   });
+
+  describe('Ruby Sass with Compass fork', function() {
+    beforeEach(function(done) {
+      sassyTest.configurePaths({
+        fixtures: path.join(__dirname, 'fixtures'),
+        includePaths: [
+          // Path to Fork version.
+          path.join(__dirname, '../fork-versions/ruby-sass-compass'),
+          // Path to normalize-scss' dependencies.
+          path.join(__dirname, '../node_modules/support-for/sass')
+        ]
+      });
+      done();
+    });
+
+    it('should render properly', function(done) {
+      sassyTest.renderFixture('fork-versions/ruby-sass-compass', {}, function(error, result, expectedOutput) {
+        should.not.exist(error);
+        done();
+      });
+    });
+  });
 });
